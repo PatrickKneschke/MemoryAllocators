4e96ddfd35c0aac3f92c673907c9428d3b99a397
Patrick Kneschke <patrick.kneschke@gmx.de>
2023-01-11 15:11:04 +0100

    Prevent creation of new FreeNode in Allocate() if remaining size is too small.

--- free_list_allocator.cpp
+++ free_list_allocator.cpp
@@ -18,8 +18,8 @@ FreeListAllocator::~FreeListAllocator() {
 void* FreeListAllocator::Allocate(const size_t size, const size_t align) {
     
     size_t requiredSize = size + sizeof(AllocHeader) + align - 1;
-    
-    // find large enough memory region for allocation
+
+    // Find memory region large enough for allocation
     FreeNode *currNode = pHead, *prevNode = nullptr;
     while (currNode && currNode->size < requiredSize)
     {
@@ -32,10 +32,24 @@ void* FreeListAllocator::Allocate(const size_t size, const size_t align) {
         throw std::overflow_error("Freelist allocator does not have a large enough memory region available.");
     }
 
-    // create a new node from remaining memory region of current node
+    // Find properly aligned address for allocation
     size_t adjustment = getAlignmentAdjustment(currNode->address + sizeof(AllocHeader), align);
     uintptr_t alignedAddress = currNode->address + adjustment + sizeof(AllocHeader);
-    FreeNode *newNode = new (reinterpret_cast<void*>(alignedAddress + size)) FreeNode(currNode->address + currNode->size - alignedAddress - size, currNode->next);
+
+    // Create a new node from remaining memory region of current node. 
+    // If remaining memory is less than 24 byte ( 16 byte header plus 8 byte data) add it to the allocated memory section instead
+    size_t newSize = currNode->address + currNode->size - alignedAddress - size;
+    size_t allocSize = size;
+    FreeNode *newNode = nullptr;
+    if(newSize >= 24)
+    {
+        newNode = new (reinterpret_cast<void*>(alignedAddress + size)) FreeNode(newSize, currNode->next);
+    }
+    else
+    {
+        allocSize += newSize;
+        newNode = currNode->next;
+    }
 
     if (prevNode == nullptr)
     {
@@ -46,13 +60,13 @@ void* FreeListAllocator::Allocate(const size_t size, const size_t align) {
         prevNode->next = newNode;
     }
 
-    mUsedMemory += newNode->address - currNode->address;
-    mMaxUsedMemory = std::max(mMaxUsedMemory, mUsedMemory);
-
-    // place allocation header in front of allocated memory section
+    // Place allocation header in front of allocated memory section
     AllocHeader *header = reinterpret_cast<AllocHeader*>(alignedAddress - sizeof(AllocHeader));
-    header->size = size;
+    header->size = allocSize;
     header->adjustment = adjustment;
+
+    mUsedMemory += header->adjustment + sizeof(AllocHeader) + header->size;
+    mMaxUsedMemory = std::max(mMaxUsedMemory, mUsedMemory);
 
     return reinterpret_cast<void*>(alignedAddress);
 }
@@ -71,7 +85,7 @@ void FreeListAllocator::Free(void* ptr) {
 
     // find adjacent nodes
     FreeNode *nextNode = pHead, *prevNode = nullptr;
-    while (nextNode->address < freeAddress)
+    while (nextNode && nextNode->address < freeAddress)
     {
         prevNode = nextNode;
         nextNode = nextNode->next;
